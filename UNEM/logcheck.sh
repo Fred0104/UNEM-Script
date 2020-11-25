2f7a61a266876e223536175756a14517391547aa
Fred0104 <74713183+Fred0104@users.noreply.github.com>
2020-11-26 02:08:44 +0800

    Add files via upload

--- UNEM/logcheck.sh
+++ UNEM/logcheck.sh
@@ -11,25 +11,32 @@ sleep 5s
 
 while true
 do
-  icount=`busybox ps -w | grep UnblockNeteaseMusic | grep -v grep | grep -v logcheck.sh`
+  	icount=`busybox ps -w | grep UnblockNeteaseMusic | grep -v grep | grep -v logcheck.sh`
 	if [ -z "$icount" ]; then
-      ./getmusicip.sh
-      ./unblockmusic.sh restart 
-  fi
+    	./getmusicip.sh
+    	./unblockmusic.sh restart 
+	fi
 	if [ ! -f "$log_file" ]; then
- 	touch "$log_file"
-	echo "create a log"
-	echo "$(date -R) # Start UnblockNeteaseMusic" >/tmp/unblockmusic.log
-  else
-	echo "$log_size"
-	log_size=`ls -l $log_file | awk '{ print $5 }'`
-	echo "$log_size"
-	echo "$log_max_size"
-	    if [ $log_size -gt $log_max_size ]; then
-	    echo "$(date -R) # Log is full,clear the log." >/tmp/unblockmusic.log
-	    logger -t "音乐解锁" "日志太大了"
-        fi
-  fi
+ 		touch "$log_file"
+		echo "create a log"
+		echo "$(date -R) # Start UnblockNeteaseMusic" >/tmp/unblockmusic.log
+  	else
+		echo "$log_size"
+		log_size=`ls -l $log_file | awk '{ print $5 }'`
+		echo "$log_size"
+		echo "$log_max_size"
+			if [ $log_size -gt $log_max_size ]; then
+				echo "$(date -R) # Log is full,clear the log." >/tmp/unblockmusic.log
+				logger -t "【音乐解锁】" "日志太大了"
+			fi
+  	fi
+	if [ $( grep "dnsmasq.music" /etc/storage/dnsmasq/dnsmasq.conf ) ]; then
+	logger -t "【音乐解锁】" "dnsmasq conf-dir is missing, trying to repair it."
+	cat >> /etc/storage/dnsmasq/dnsmasq.conf << EOF
+conf-dir=/tmp/dnsmasq.music
+EOF
+	fi
+	
 	echo "check"
 	sleep 29s
 done
